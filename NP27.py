97f4f49eb3de0e744285b0f96700801a059912af
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-17 19:29:21 +0100

    Create NP27.py

---
+++ NP27.py
@@ -0,0 +1,8 @@
+name = ['Niumei', 'YOLO', 'Niu Ke Le', 'Mona'] 
+food = ['pizza', 'fish', 'potato', 'beef']
+friends = []
+num = [3, 6, 0, 3]
+
+for i in name, food, num:
+    friends.append(i)
+print(friends)
