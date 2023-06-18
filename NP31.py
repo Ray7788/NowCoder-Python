2ec49ca4f60781850ac0d1cb47f1b8a9d45989d7
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-18 14:49:22 +0100

    Create NP31.py

---
+++ NP31.py
@@ -0,0 +1,6 @@
+group_list =  ['Tom', 'Allen', 'Jane', 'William', 'Tony']
+print(group_list[0:2])  # 前2个
+print(group_list[1:4])  # 中间3个
+print(group_list[-2:])  # 后2个
+
+# print("{}\n{}\n{}".format(group_list[:2],group_list[1:4],group_list[-2:]))
