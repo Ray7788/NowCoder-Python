c33b318d0a30439ce137c512c0e757518c7d7cfe
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-18 15:18:32 +0100

    Update NP37.py

--- NP37.py
+++ NP37.py
@@ -1,5 +1,6 @@
 k,x,y = input().split()
 print(float(k) <= float(x))
 print(float(k) >= float(y))
-# k, x, y = map(float,input().split())
+
+# k, x, y = map(float,input().split())  # 映射 map() 函数会根据提供的函数对指定序列做映射。
 # print(k <= x, k >= y, sep = "\n")
