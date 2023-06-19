51b8dc239d3c36feefa82ddfbf2caa125072ec39
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-19 12:59:58 +0100

    Create NP46.py

---
+++ NP46.py
@@ -0,0 +1,21 @@
+name = input()
+
+if name == 'pizza':
+    print(10)
+elif name == 'rice':
+    print(2)
+elif name == 'yogurt':
+    print(5)
+else:
+    print(8)
+
+# dish = {'pizza':10, 'rice':2, 'yogurt':5, 'others':8}
+# x = input()
+
+# if x not in dish.keys():
+#     x = 'others'    # 将没有的keys 都归到 others里
+    
+# for i in dish.keys():
+#     if i == x:
+#         print(dish[i])
+#         break
