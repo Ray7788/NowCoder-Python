23b32ae011fba36799fcf855084e6a21afa9a97e
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-20 16:10:26 +0100

    Create NP58.py

---
+++ NP58.py
@@ -0,0 +1,7 @@
+user_list = ['Niuniu','Niumei','HR','Niu Ke Le','GURR','LOLO']
+
+for i in user_list:
+    if i == "HR":
+        print('Hi, HR! Would you like to hire someone?')
+    else:
+        print('Hi, {}! Welcome to Nowcoder!'.format(i))
