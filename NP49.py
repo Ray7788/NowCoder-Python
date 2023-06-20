91c743a02c1cfd7dfee6a39fb6b20bda2edc5f18
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-20 17:05:48 +0100

    增加注释

--- NP49.py
+++ NP49.py
@@ -1,5 +1,5 @@
 my_list = ['P', 'y', 't', 'h', 'o', 'n']
 print('Here is the original list:\n {}'.format(my_list))
 
-print()
+print() # 为了空行
 print('The number that my_list has is:\n {}'.format(len(my_list)))
