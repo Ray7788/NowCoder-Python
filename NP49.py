fb42fb423c2084ee7e8f11d5fa758d2d079cb005
Rui(Ray) Xu <ray778@foxmail.com>
2023-06-22 16:22:15 +0100

    删除多余的会报错的空格

--- NP49.py
+++ NP49.py
@@ -1,5 +1,5 @@
 my_list = ['P', 'y', 't', 'h', 'o', 'n']
-print('Here is the original list:\n {}'.format(my_list))
+print('Here is the original list:\n{}'.format(my_list))
 
 print() # 为了空行
-print('The number that my_list has is:\n {}'.format(len(my_list)))
+print('The number that my_list has is:\n{}'.format(len(my_list)))
